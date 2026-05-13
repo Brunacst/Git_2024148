01ec898deb646cb7d0cecb5638a11f30e246660a
Brunacst <2024148@student.cct.ie>
2026-05-14 00:04:14 +0100

    Added a simple print with my name and my student number!

--- src/git_2024148/Git_2024148.java
+++ src/git_2024148/Git_2024148.java
@@ -14,7 +14,7 @@ public class Git_2024148 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bruna Teixeira - 2024148");
     }
     
 }
